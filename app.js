f60b161bf2871fc36ba812fb012fe1766348c9bc
nmuchiri <lonesomebounty@gmail.com>
2020-10-02 13:50:35 -0400

    game done

--- app.js
+++ app.js
@@ -1,20 +1,52 @@
 // we will store the card values below at the beginning of the game
 
 let cardFlipped= false
-let firstCard= null
-let secondCard= null
+  let firstCard= null
+  let secondCard= null
+  let count = 0
 
+score= document.querySelector(".score")
+console.log(score)
+timer= document.querySelector(".timer")
 
 cards= document.querySelectorAll(".card")
+let second=0
+let minute=0
+let hour=0
 // console.log(cards)
+
+function startGame(){
+  location.reload()
+  timer.innerHTML=0
+
+}
+
+
+function startTimer(){
+  interval = setInterval(function(){
+      timer.innerHTML = minute+"mins "+second+"secs";
+      second++;
+      if(second === 60){
+          minute++;
+          second = 0;
+      }
+      if(minute === 60){
+          hour++;
+          minute = 0;
+      }
+  },1000);
+}
+
+startTimer()
 
  function flipCard () {
 
     if(this === firstCard){
+        // console.log("line 13",this)
         return // prevents the function from removing event listener incase of double clicking the same card
     }
     this.classList.toggle('flip');
-    // console.log(this)// here this gives you the div of the object clicked
+    // console.log("flipCard.this",this)// here this gives you the div of the object clicked
     // console.log(e.target)// this targets the particular image in the div while this targets the div itself- which has two images within it
     if(!cardFlipped){
         cardFlipped= true // the first card is flipped and cardFlipped is true
@@ -42,23 +74,26 @@ cards= document.querySelectorAll(".card")
     if(firstCard.dataset.id === secondCard.dataset.id){
         firstCard.removeEventListener ("click", flipCard)
         secondCard.removeEventListener ("click", flipCard)
-       
-        resetGame()
     }else{
 
         // when its not a match remove the flip function so that the cards can flip back again. The set time out function makes sure that they don't flip back immediately
         setTimeout(() => {
             firstCard.classList.remove('flip')
             secondCard.classList.remove('flip')
-            resetGame()
           }, 1000);
 
         } 
+        if(firstCard.dataset.id === secondCard.dataset.id){
+          count++   
+          score.innerHTML = count
+        // alert("Congratulations!! You've found match. Your count is " + count)
+        }
 
     }
 
+    
   }
-
+// console.log(flipCard)
 
   function shuffleCards (){
     cards.forEach(cards=> {
@@ -68,24 +103,13 @@ cards= document.querySelectorAll(".card")
     })
 
 }
-
 shuffleCards()
-
-  function resetGame(){
-    [cardFlipped= false];
-     [firstCard,secondCard = null, null];
-    
-
-}// reset game resets everything to the way it was before the game was played
-
-resetGame()
-console.log(resetGame)
 
 document.addEventListener("DOMContentLoaded", ()=>{
 
     button= document.querySelector(".ResetBoard")
     // console.log(button)
-    button.addEventListener ("click", resetGame)
+    button.addEventListener ("click", startGame)
   
     cards.forEach(cards => cards.addEventListener("click", flipCard))// added a click event for both cards using forEach method
    
