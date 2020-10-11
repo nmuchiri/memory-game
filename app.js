067c18fae48e48e8f10263fda89f2120dfbf660a
nmuchiri <lonesomebounty@gmail.com>
2020-10-11 13:40:04 -0400

    added winning game module

--- app.js
+++ app.js
@@ -1,15 +1,28 @@
 // we will store the card values below at the beginning of the game
 
-let cardFlipped= false
+  let cardFlipped= false
   let firstCard= null
   let secondCard= null
   let count = 0
-  let second=60
-  // let minute=0
-  // let hour=0
+  let second=30
+
+  const score= document.querySelector(".score")
+  const timer= document.querySelector(".timer")
+  const cards= document.querySelectorAll(".card")
+  const modal = document.querySelector("#my-modal")
+  const modalBtn = document.querySelector("#modal-btn")
+  const closeBtn = document.querySelector(".close")
+  const closeWin = document.querySelector(".win-close")
+  const winningModal=document.querySelector("#win-modal")
+  const losingModal=document.querySelector("#lose-modal")
   
 
+  modalBtn.addEventListener("click", openModal)
+  closeBtn.addEventListener("click", closeModal)
+  window.addEventListener("click", outsideClick)
+  closeWin.addEventListener("click", closingWin)
 
+   
   function gameAudio(){
     let myAudio = document.createElement("audio")
     myAudio.src = "Memory Game Images/Professor Layton and the Last Time Travel OST The Professor's Trunk (Minicar) (HQ Version).mp3";
@@ -17,98 +30,96 @@ let cardFlipped= false
     myAudio.play()
     
   }
-  gameAudio()
+  
+  
+  function openModal() {
+    modal.style.display = "block"
+    startTimer()
+    gameAudio()
+  }
+
+  function winModal() {
+    winningModal.style.display = "block"
+  }
+  function closingWin() {
+    winningModal.style.display = "none"
+    startGame()
+  }
+  function loseModal() {
+    
+      losingingModal.style.display = "block"
+  }
 
 
-score= document.querySelector(".score")
-console.log(score)
-timer= document.querySelector(".timer")
+  function closeModal() {
+    modal.style.display = "none"
+    
+  }
 
-cards= document.querySelectorAll(".card")
+  function outsideClick(e) {
+    if (e.target == modal) {
+      modal.style.display = "none"
+    }
+  }
 
-// console.log(cards)
+
 
 function startGame(){
-  location.reload()
-  timer.innerHTML=60
-  
+  location.reload() 
 }
-
-
-
 
 function startTimer(){
   interval = setInterval(function(){
-       timer.innerHTML = /*minute+"mins "*/ +second+"secs";
+       timer.innerHTML = second+"secs"
       second--;
       if(second === 0){
-      startGame()
+      // startGame()
       }
-      // if(minute === 60){
-      //     hour++;
-      //     minute = 0;
-      // }
-  },1000);
+        },1200)
 }
 
-startTimer()
+
 
  function flipCard () {
 
     if(this === firstCard){
-        // console.log("line 13",this)
-        return // prevents the function from removing event listener incase of double clicking the same card
+        return 
     }
+    
     this.classList.toggle('flip');
-    // console.log("flipCard.this",this)// here this gives you the div of the object clicked
-    // console.log(e.target)// this targets the particular image in the div while this targets the div itself- which has two images within it
     if(!cardFlipped){
-        cardFlipped= true // the first card is flipped and cardFlipped is true
-        firstCard= this // the element that has fired the event
+        cardFlipped= true
+        firstCard= this 
         console.log(firstCard)
     }else{ // 
-        cardFlipped= false// card flipped is switched back to false to prepare for the second click
-        // switch this to the second card so that it identifies the next click as second card
+        cardFlipped= false
         secondCard= this
         console.log(cardFlipped, secondCard)
 
-       
-
- // check if the cards match by comparing their data set attributes
-    // add data attribute to the pictures in HTML dataset is a method for getting the data of the first and second cards you can also use getAttribute according to MDN- line 38. 
-
-    // when the player clicks the first card or the second card we have know which cards they are so we can match them
-
-    // console.log(firstCard.dataset.id)
-    // console.log(this.getAttribute("data-id"))
-    // this will give the name on the data-set from HTML eg  hat10
-    
-
-    // if the data attributes of the cards match then remove the cards by removing the event listener
     if(firstCard.dataset.id === secondCard.dataset.id){
         firstCard.removeEventListener ("click", flipCard)
         secondCard.removeEventListener ("click", flipCard)
     }else{
-
-        // when its not a match remove the flip function so that the cards can flip back again. The set time out function makes sure that they don't flip back immediately
         setTimeout(() => {
             firstCard.classList.remove('flip')
             secondCard.classList.remove('flip')
-          }, 1000);
-
+          }, 1000)
+        
         } 
         if(firstCard.dataset.id === secondCard.dataset.id){
           count++   
           score.innerHTML = count
-        // alert("Congratulations!! You've found match. Your count is " + count)
 
-        }
-
+    }if(count === 6){
+      winModal()
+    }if(count < 6 && second === 0){
+      alert("you lose!")
+      loseModal()
     }
-
     
   }
-// console.log(flipCard)
+
+ }
 
 
   function shuffleCards (){
@@ -119,22 +130,12 @@ startTimer()
     })
 
 }
-shuffleCards()
-//////////////////////////////////////////////////////////
-// adding sounds to cards
-function play(){
-  audio = document.getElementById("audio") // managed to add sound to one card will find out how to do it for all cards
-  audio.play();
-            }
-
 
 
 document.addEventListener("DOMContentLoaded", ()=>{
 
     button= document.querySelector(".ResetBoard")
-    // console.log(button)
     button.addEventListener ("click", startGame)
-  
-    cards.forEach(cards => cards.addEventListener("click", flipCard))// added a click event for both cards using forEach method
-   
+    cards.forEach(cards => cards.addEventListener("click", flipCard))
+    // shuffleCards()
 })
